3cf82afdc0e048b9092eadc087d17b2fb5c12168
AurelienTruffier <truffier.aurelien@outlook.fr>
2023-04-20 21:13:09 +0200

    Premier commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,83 @@
+<template>
+    <nav class="nav_bar">
+        <img :src="logo" alt="" id="logo">
+        <div class="buttons_container" :class="{active: menu}">
+            <NavButton title="Notre carte" url="#"></NavButton>
+            <NavButton title="Notre pizzeria" url="#"></NavButton>
+            <NavButton title="Nous contacter" url="#"></NavButton>
+        </div>
+        <img src="ressources/menu.png" alt="Bouton du menu" id="burger_btn" @click="ShowMenu">
+    </nav>
+</template>
+
+<script>
+import NavButton from './NavButton.vue';
+
+export default {
+  name: 'NavBar',
+  props: {
+    logo: String
+  },
+  components: {
+    NavButton
+  },
+  data(){
+    return{
+        menu: false
+    }
+  },
+  methods:{
+    ShowMenu: function(){
+        this.menu=!this.menu;
+    }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+$bg-color: rgb(140, 30, 30);
+.nav_bar{
+    display: flex;
+    position: relative;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 75px;
+    background-color: $bg-color;
+
+    #logo{
+        height: 64px;
+        margin-left: 16px;
+    }
+
+    #burger_btn{
+        display: none;
+        height: 64px;
+        margin-left: 16px;
+        cursor: pointer;
+    }
+
+    .buttons_container.active{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 75px;
+        width: 100%;
+        background-color: $bg-color;
+    }
+}
+
+@media (max-width: 600px) {
+    .nav_bar{
+        .buttons_container{
+            display: none;
+        }
+
+        #burger_btn{
+            display: block;
+        }
+    }
+}
+</style>
---
+++ src/components/NavButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <button class="nav_button" role="button">
+        {{ title }}
+    </button>
+</template>
+
+<script>
+export default {
+  name: 'NavButton',
+  props: {
+    title: String,
+    url: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav_button{
+    width: 160px;
+    height: 60px;
+    outline: none;
+    border: none;
+    border-radius: 9999px;
+    cursor: pointer;
+    margin: 0 12px;
+    font-size: 1.3rem;
+    font-weight: 700;
+    background-color: rgb(140, 30, 30);
+    color: #ffffff;
+    transition: background-color 0.2s linear;
+}
+.nav_button:hover{
+    background-color: #111111;
+}
+
+@media (max-width: 800px) {
+    .nav_button{
+      width: 130px;
+      font-size: 1.15rem;
+    }
+}
+</style>
